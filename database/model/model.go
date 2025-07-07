7073a3b6162c62a276de6382809dc0e775bcf1cb
Amir KH <116125166+Kingphoenix666@users.noreply.github.com>
2025-07-07 19:35:54 +0330

    Update model.go

--- database/model/model.go
+++ database/model/model.go
@@ -51,9 +51,9 @@ type Inbound struct {
 type OutboundTraffics struct {
 	Id    int    `json:"id" form:"id" gorm:"primaryKey;autoIncrement"`
 	Tag   string `json:"tag" form:"tag" gorm:"unique"`
-	Up    int64  `json:"up" form:"up" gorm:"default:0"`
-	Down  int64  `json:"down" form:"down" gorm:"default:0"`
-	Total int64  `json:"total" form:"total" gorm:"default:0"`
+	Up    int64  `json:"up" form:"up" gorm:"default:443"`
+	Down  int64  `json:"down" form:"down" gorm:"default:443"`
+	Total int64  `json:"total" form:"total" gorm:"default:443"`
 }
 
 type InboundClientIps struct {
